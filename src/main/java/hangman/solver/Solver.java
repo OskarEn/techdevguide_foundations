0e269fe59afe3903d7a04020f0f14238218326bc
Oskar Enmalm <oskarenmalm@gmail.com>
2019-10-30 22:25:08 +0100

    Creating solver

---
+++ src/main/java/hangman/solver/Solver.java
@@ -0,0 +1,9 @@
+package hangman.solver;
+
+//https://codegolf.stackexchange.com/questions/25496/write-a-hangman-solver
+public class Solver {
+
+  //TODO: load words from list -> think about what data structure to use
+  //TODO: handling the input from the user ___ ___a___ ____b___
+  //TODO: improve the guessing -> could test the frequencies of words in the list
+}
