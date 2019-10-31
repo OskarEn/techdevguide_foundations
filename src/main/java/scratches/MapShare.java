921c5cbb39119ace1c5aa6f0fefdd9038d78f5dc
Oskar Enmalm <oskarenmalm@gmail.com>
2019-10-31 20:40:07 +0100

    I didn't read properly but the conditions are ambiguous... don't want to change it anymore - I've learnt more map methods than what the question needed

--- src/main/java/scratches/MapShare.java
+++ src/main/java/scratches/MapShare.java
@@ -4,17 +4,33 @@ package scratches;
 //Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have
 //that same value. In all cases remove the key "c", leaving the rest of the map unchanged.
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapShare {
-
+  mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
+  mapShare({"a": "aaa", "c": "meh", "d": "hi"}) → {"a": "aaa", "b": "aaa", "d": "hi"}
   public static void main(String[] args) {
-    mapShare({"b":"xyz","c":"ccc"}) //→ {"b":"xyz"}
-    mapShare({"a":"aaa","b":"bbb","c":"ccc"})// → {"a":"aaa","b":"aaa"}
-    mapShare({"a":"aaa","c":"meh","d":"hi"})// → {"a":"aaa","b":"aaa","d":"hi"}
+    Map<String, String> map = new HashMap<>();
+    map.put("b", "xyz");
+    map.put("c", "ccc");
+    System.out.println(mapShare(map)); //→ {"b":"xyz"}
+
+    map.clear();
+    map.put("a", "aaa");
+    map.put("b", "bbb");
+    map.put("c", "ccc");
+    System.out.println(mapShare(map));// → {"a":"aaa","b":"aaa"}
+
+    map.clear();
+    map.put("a", "aaa");
+    map.put("c", "meh");
+    map.put("d", "hi");
+    System.out.println(mapShare(map));// → {"a":"aaa","b":"aaa","d":"hi"}
   }
 
-  public static Map<String, String> mapShare(Map<String, String> orig) {
+  public static Map<String, String> mapShare(Map<String, String> map) {
+    Map<String, String> orig = map;
     if(orig == null || orig.isEmpty())
       return orig;
 
@@ -22,30 +38,14 @@ public class MapShare {
     boolean hasA = containsNotEmpty(orig, "a");
     boolean hasB = containsNotEmpty(orig, "b");
 
-    if(!hasA && !hasB)
-      return orig;
-
-    //There is definitely a more elegant way
-    if(hasA ^ hasB) {
-      if(hasA) {
-        orig.put("b", orig.get("a"));
-        orig.remove("a");
-      } else {
-        orig.put("a", orig.get("b"));
-        orig.remove("b");
-      }
-
-      return orig;
+    if(hasA && hasB) {
+      //There is definitely a more elegant way
+      String a = orig.get("a");
+      String b = orig.get("b");
+      orig.replace("a", b);
+      orig.replace("b", a);
     }
-
-    //There is definitely a more elegant way
-    String a = orig.get("a");
-    String b = orig.get("b");
-    orig.replace("a", b);
-    orig.replace("b", a);
     return orig;
-
-    orig.computeIfAbsent("b", () -> {})
   }
 
   private static boolean containsNotEmpty(Map<String, String> orig, String a) {
